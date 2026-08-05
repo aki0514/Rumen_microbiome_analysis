6efab25a0d3e71d7ee0cdb6bd0304ac71466677c
aki0514 <53364171+aki0514@users.noreply.github.com>
2026-08-05 17:00:52 +0900

    Add files via upload

---
+++ coverm_MAG.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Usage:
+#   bash coverm_MAG.sh <GenomeDir> <ForwardReads> <ReverseReads> <OutputDir> <SampleName>
+#
+# <GenomeDir>    : directory containing dereplicated MAGs (*.fa)
+# <ForwardReads> : forward paired-end reads (FASTQ.gz)
+# <ReverseReads> : reverse paired-end reads (FASTQ.gz)
+# <OutputDir>    : output directory
+# <SampleName>   : sample name
+
+GENOMES="$1"
+READ1="$2"
+READ2="$3"
+OUTDIR="$4"
+SAMPLE="$5"
+
+mkdir -p "${OUTDIR}"
+
+coverm genome \
+    -1 "${READ1}" \
+    -2 "${READ2}" \
+    --genome-fasta-directory "${GENOMES}" \
+    -x fa \
+    -m rpkm tpm \
+    -p bwa-mem \
+    --threads 16 \
+    --min-covered-fraction 10 \
+    --min-read-percent-identity 95 \
+    --min-read-aligned-percent 75 \
+    > "${OUTDIR}/${SAMPLE}_coverm.tsv"
